e0dcb684972f534c769dbd2fded44f4ca230af6f
nimadaryabar <nima.daryabar@gmail.com>
2025-03-10 15:39:24 +0100

    refactor: Modular implementation by breaking the code into small functions

--- app.py
+++ app.py
@@ -37,9 +37,8 @@ def preprocessing_frame(frame):
     return gray_frame
 
 
-
-if __name__ == "__main__":
-
+# Create a function to load the haarcascade classifier
+def haarcascade_classifier():
     # load haarcascade classifier for face detection
     face_cascade_classifier = cv2.CascadeClassifier(
         cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
@@ -49,17 +48,62 @@ if __name__ == "__main__":
     if face_cascade_classifier.empty():
         print("Face cascade classifier is empty")
         sys.exit()
+
+    return face_cascade_classifier
+
+
+def face_detector(classifier, frame):
+    # Detect faces in the image
+    faces = classifier.detectMultiScale(
+        frame,
+        scaleFactor=1.1, # scale down the image by 20% to detect larger faces
+        minNeighbors=5, # number of neighboring rectangles (lower -> more false positives)
+        minSize=(30, 30), # minimum size of the face to detect
+        maxSize=(300, 300)  # Ignore very large faces
+    )
+
+    return faces
+
+
+# To scale the bounding box back to the original size
+def scale_bounding_box(frame, detected_faces):
+    # Get the original height and width of the frame to rescale the bounding box
+    original_height, original_width = frame.shape[:2]
+
+    # Scale bounding boxes back to original size
+    scale_x = original_width / 300
+    scale_y = original_height / 300
+
+    # Store the scaled coordinates of the detected faces
+    scaled_faces = []
+    for (x, y, w, h) in detected_faces:
+        x = int(x * scale_x)
+        y = int(y * scale_y)
+        w = int(w * scale_x)
+        h = int(h * scale_y)
+        scaled_faces.append((x, y, w, h))
     
+    return scaled_faces
+
+
+def draw_rectangle(frame, detected_faces):
+    # Store the scaled coordinates of the detected faces
+    scaled_faces = scale_bounding_box(frame, detected_faces)
+
+    # Draw a bounding box around the detected faces
+    for (x, y, w, h) in scaled_faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+
+if __name__ == "__main__":    
     # Create a video capture object
     cap = video_capture()
+    classifier = haarcascade_classifier()
 
     # Continuously read the frames from the webcam
     while True:
         # Read a frame
         res, frame = cap.read()
-
-        # Get the original height and width of the frame to rescale the bounding box
-        original_height, original_width = frame.shape[:2]
 
         # If the frame is not captured, then break the loop
         if not res:
@@ -68,32 +112,15 @@ if __name__ == "__main__":
 
         # Preprocess the frame
         processed_frame = preprocessing_frame(frame)
+        
+        # Haarcascade clasifier object
+        classifier = haarcascade_classifier()
 
-        # Detect faces in the image
-        faces = face_cascade_classifier.detectMultiScale(
-            processed_frame,
-            scaleFactor=1.1, # scale down the image by 20% to detect larger faces
-            minNeighbors=5, # number of neighboring rectangles (lower -> more false positives)
-            minSize=(30, 30), # minimum size of the face to detect
-            maxSize=(300, 300)  # Ignore very large faces
-        )
-
-        # Scale bounding boxes back to original size
-        scale_x = original_width / 300
-        scale_y = original_height / 300
-
-        # Store the scaled coordinates of the detected faces
-        scaled_faces = []
-        for (x, y, w, h) in faces:
-            x = int(x * scale_x)
-            y = int(y * scale_y)
-            w = int(w * scale_x)
-            h = int(h * scale_y)
-            scaled_faces.append((x, y, w, h))
+        # Detect faces in the frame
+        detected_faces = face_detector(classifier, processed_frame)
 
         # Draw a bounding box around the detected faces
-        for (x, y, w, h) in scaled_faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        draw_rectangle(frame, detected_faces)
 
         # Display the captured frame
         cv2.imshow("Webcam", frame)
