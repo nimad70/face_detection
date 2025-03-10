f5850c40e7c1a06ae446ca279530026a64c185d5
nimadaryabar <nima.daryabar@gmail.com>
2025-03-10 18:21:24 +0100

    refactor: Refined app.py by using docstrings and removing redundent comments

--- app.py
+++ app.py
@@ -3,39 +3,34 @@ import sys
 from src.face_detection import video_capture, haarcascade_classifier, face_detector, draw_rectangle, preprocessing_frame
 
 
-# To start the face detection process
 def main():
-    # Create a video capture object
+    """
+    Captures video from the webcam, detects faces, and displays the processed frames with bounding boxes from 
+    face_detection.py module.
+    
+    The function continuously reads frames from the webcam, preprocesses them, detects faces using Haar Cascade classifier,
+    and draws bounding boxes around detected faces. The video stream is displayed in a window, and the loop exits
+    when the user presses 'q'.
+    """
     cap = video_capture()
     classifier = haarcascade_classifier()
 
-    # Continuously read the frames from the webcam
     while True:
-        # Read a frame
-        res, frame = cap.read()
+        res, frame = cap.read() # capture frames from the video source
 
         # If the frame is not captured, then break the loop
         if not res:
             print("Fail to capture frames")
             break
 
-        # Preprocess the frame
         processed_frame = preprocessing_frame(frame)
-        
-        # Haarcascade clasifier object
         classifier = haarcascade_classifier()
-
-        # Detect faces in the frame
         detected_faces = face_detector(classifier, processed_frame)
-
-        # Draw a bounding box around the detected faces
         draw_rectangle(frame, detected_faces)
 
-        # Display the captured frame
-        cv2.imshow("Webcam", frame)
-        # cv2.imshow("Webcam", gray_frame)
+        cv2.imshow("Webcam", frame) # Display the captured frame
 
-        # Exit the program if the user presses 'q'
+        # Exit if the user presses 'q'
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
