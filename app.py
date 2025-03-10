9ae0aeab533048a85b458c4d6cbee4a3f9479ce5
nimadaryabar <nima.daryabar@gmail.com>
2025-03-10 14:00:17 +0100

    feat: Implemented image processing steps,resizing, gray scale, preprocessing

--- app.py
+++ app.py
@@ -1,37 +1,67 @@
 import cv2
 import sys
 
-# Open the first available webcam
-cap = None
-for i in range(5):
-    cap = cv2.VideoCapture(i)
-    # Check if the webcam is opened
-    if cap.isOpened():
-        print(f"Using webcam number: {i}")
-        break
+
+# Create a VideeCapture function to capture the video from the webcam
+def video_capture():
+    # Open the first available webcam
+    cap = None
+    for i in range(5):
+        cap = cv2.VideoCapture(i)
+        # Check if the webcam is opened
+        if cap.isOpened():
+            print(f"Using webcam number: {i}")
+            break
+        
+    # In case no webcam is found to exit the program
+    if not cap or not cap.isOpened():
+        print("No webcam found!")
+        sys.exit()
     
-# In case no webcam is found to exit the program
-if not cap or not cap.isOpened():
-    print("No webcam found!")
-    sys.exit()
+    return cap
 
-# Continuously read the frames from the webcam
-while True:
-    # Read a frame
-    res, frame = cap.read()
 
-    # If the frame is not read, exit the program
-    if not res:
-        print("Fail to capture frames")
-        break
+def preprocessing_frame(frame):
+    # Resize the frame to 300x300 for faster processing
+    resized_frame = cv2.resize(frame, (300, 300))
 
-    # Display the captured frame
-    cv2.imshow("Webcam", frame)
+    # Convert the image to grayscale for computational efficiency
+    gray = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
 
-    # Exit the program if the user presses 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # To enhance contrast
+    gray = cv2.equalizeHist(gray)
 
-# Release the webcam and close the window
-cap.release()
-cv2.destroyAllWindows()
+    # To remove noises
+    gray = cv2.GaussianBlur(gray, (3, 3), 0)
+
+    return gray
+
+
+
+if __name__ == "__main__":
+    
+    cap = video_capture()
+
+    # Continuously read the frames from the webcam
+    while True:
+        # Read a frame
+        res, frame = cap.read()
+
+        # If the frame is not captured, then break the loop
+        if not res:
+            print("Fail to capture frames")
+            break
+
+        gray_frame = preprocessing_frame(frame)
+
+        # Display the captured frame
+        # cv2.imshow("Webcam", frame)
+        cv2.imshow("Webcam", gray_frame)
+
+        # Exit the program if the user presses 'q'
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the webcam and close the window
+    cap.release()
+    cv2.destroyAllWindows()
