0701dc567d0045e47411c26b5a6db27ee4482136
nimadaryabar <nima.daryabar@gmail.com>
2025-03-11 20:19:39 +0100

    feat: Implemeneted resize_images() to resize and sclae images in data_augmentation.py

--- src/data_augmentation.py
+++ src/data_augmentation.py
@@ -2,10 +2,38 @@
 Data Augmentation for Image Classification
 """
 
+from pathlib import Path
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+
+# Path contants
+DATASET_PATH = Path("dataset")
+DATA_PATH = Path("data")
+
+IMG_SIZE = 180
+
+def resize_images(image):
+    """
+    Resize images to the IMG_SIZE constant for shape consistent and rescale pixel values.
+    """
+    resize_and_rescale = tf.keras.Sequential([
+        layers.Resizing(IMG_SIZE, IMG_SIZE), 
+        layers.Rescaling(1./255)
+    ])
+    result = resize_and_rescale(image)
+    return result
+
+
+
+
+
+
+
 
 
