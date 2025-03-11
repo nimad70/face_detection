8506d133331d287c864c4bf3cebeadd997b4217e
nimadaryabar <nima.daryabar@gmail.com>
2025-03-11 21:29:12 +0100

    feat: Implemented save_augmented_images() to save the augmented images into the corresponding training directory [smile/nosmile]

--- src/data_augmentation.py
+++ src/data_augmentation.py
@@ -2,6 +2,8 @@
 Data Augmentation for Image Classification
 """
 
+import random
+import time
 from pathlib import Path
 import numpy as np
 import cv2
@@ -18,19 +20,23 @@ DATA_PATH = Path("data")
 
 # Directories for train/validation/test datasets
 TRAIN_DATA_PATH = DATA_PATH / "train"
-VAL_DATA_PATH = DATA_PATH / "val"
-TEST_DATA_PATH = DATA_PATH / "test"
+# VAL_DATA_PATH = DATA_PATH / "val"
+# TEST_DATA_PATH = DATA_PATH / "test"
 LABELS = ["smile", "nosmile"]
 
 # Create directories if they do not exist already
-for dirs in [TRAIN_DATA_PATH, VAL_DATA_PATH, TEST_DATA_PATH]: 
-    for label in LABELS:
-        (dirs / label).mkdir(parents=True, exist_ok=True)
+# for dirs in [TRAIN_DATA_PATH, VAL_DATA_PATH, TEST_DATA_PATH]: 
+#     for label in LABELS:
+#         (dirs / label).mkdir(parents=True, exist_ok=True)
+
+for label in LABELS:
+        (TRAIN_DATA_PATH / label).mkdir(parents=True, exist_ok=True)
 
 
 IMG_SIZE = 180
 
-def resize_images(image):
+# save time, memory, and computing resources by resizing and rescaling images before training
+def resize_rescale_images(image):
     """
     Resize images to the IMG_SIZE constant for shape consistent and rescale pixel values.
     """
@@ -42,6 +48,7 @@ def resize_images(image):
     return result
 
 
+# Apply data augmentation to the input image
 def data_augmentation(image):
     """
     Apply data augmentation to the input image.
@@ -57,7 +64,37 @@ def data_augmentation(image):
     return result
 
 
+# Save augmented images to the training dataset
+def save_augmented_images():
+    for label in LABELS:
+        image_directory = DATASET_PATH / label
 
+        # valid_extensions = [".jpg", ".jpeg", ".png"]
+        # all_images = [img for img in image_directory.iterdir() if img.suffix in valid_extensions]
+        all_images = list(image_directory.iterdir())
+        
+        # Augment 50% of images
+        half_augment = len(all_images) // 2 
+        
+        # Randomly select images to augment
+        selected_images = random.sample(all_images, half_augment)
 
+        for image_path in selected_images:  # Select only half of the images
+            image = cv2.imread(str(image_path))
+            image = resize_rescale_images(image)
 
+            # Skip unreadable images
+            if image is None:
+                continue
 
+            # Add batch dimension
+            image = np.expand_dims(img, axis=0)
+            augmented_image = data_augmentation(image)
+            
+            # Convert back to the original shape
+            augmented_image = np.squeeze(augmented_image.numpy().astype("uint8"))
+
+            # Save augmented images
+            filename = f"face_{int(time.time())}.jpg"
+            aug_path = TRAIN_DATA_PATH / label / filename
+            cv2.imwrite(str(aug_path), augmented_image)
