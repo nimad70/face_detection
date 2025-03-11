e12aafc1b56719bdbd70768e4a87cfeda10dc721
nimadaryabar <nima.daryabar@gmail.com>
2025-03-12 00:01:30 +0100

    feat: Built train/val/test datasets in model_training.py

--- src/model_training.py
+++ src/model_training.py
@@ -25,6 +25,28 @@ for dirs in [TRAIN_DATA_PATH, VAL_DATA_PATH, TEST_DATA_PATH]:
     for label in LABELS:
         (dirs / label).mkdir(parents=True, exist_ok=True)
 
-IMG_SIZE = 300
+IMG_SIZE = 256
 BATCH_SIZE = 32
 
+
+# Loading data
+train_ds = keras.preprocessing.image_dataset_from_directory(
+    str(TRAIN_DATA_PATH),
+    image_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE
+)
+
+val_ds = keras.preprocessing.image_dataset_from_directory(
+    str(VAL_DATA_PATH),
+    image_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE
+)
+
+test_ds = keras.preprocessing.image_dataset_from_directory(
+    str(TEST_DATA_PATH),
+    image_size=(IMG_SIZE, IMG_SIZE),
+    batch_size=BATCH_SIZE
+)
+
+class_names = train_ds.class_names
+print("Class Names:", class_names)
