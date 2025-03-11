bd86ee69fa80bf98ee7eafe27da718bed27497f3
nimadaryabar <nima.daryabar@gmail.com>
2025-03-11 17:50:39 +0100

    feat: Initialized data_augmentation.py to apply data augmentation the training set for model training

---
+++ src/data_augmentation.py
@@ -0,0 +1,11 @@
+"""
+Data Augmentation for Image Classification
+"""
+
+import numpy as np
+import cv2
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
