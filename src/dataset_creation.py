84fbb1f582430bb40ac4591b8f3e40e60b97506a
nimadaryabar <nima.daryabar@gmail.com>
2025-03-11 15:45:50 +0100

    refactor: Added comments, docstrings are Declared in dataset_creation.py file

--- src/dataset_creation.py
+++ src/dataset_creation.py
@@ -1,49 +1,58 @@
-import sys
-import cv2
-import time
-import csv
+"""
+Dataset Splitting Script
+
+This script automates the splitting of image data into training, validation, and testing datasets. 
+It is specifically designed for a binary classification task to organize data into structured directories for easy access during image processing model training.
+"""
+
 from pathlib import Path
 import shutil
 import random
 
 
-# AUtomate spliting the data
 # Path Constants
 DATASET_PATH = Path("dataset")
 DATA_PATH = Path("data")
 
-# To create train/val/test datasets
+# Directories for train/validation/test datasets
 TRAIN_DATA_PATH = DATA_PATH / "train"
 VAL_DATA_PATH = DATA_PATH / "val"
 TEST_DATA_PATH = DATA_PATH / "test"
 LABELS = ["smile", "nosmile"]
 
-
+# Create directories if they do not exist already
 for dirs in [TRAIN_DATA_PATH, VAL_DATA_PATH, TEST_DATA_PATH]: 
     for label in LABELS:
         (dirs / label).mkdir(parents=True, exist_ok=True)
 
-
-# To split the dataset
 def split_dataset(labels):
+    """
+    Splits images into training, validation, and test sets based on predefined ratios.
+
+    Args:
+        labels (list): List of labels or categories to split (e.g., ['smile', 'nosmile']).
+
+    Returns:
+        bool: True if any category has insufficient data to split; otherwise, False.
+    """
     is_small_to_split = False
 
-    # To define the ratio of the dataset
     for label in labels:
+        # Fetch and shuffle image files from the dataset directory
+        # Define the split ratios
         train_ratio = 0.6
         val_ratio = 0.2
         # test_ratio = 0.2
 
-        # To get the image files with from the asked directory
-        image_files = list((DATASET_PATH / label).glob("*.jpg"))
+        image_files = list((DATASET_PATH / label).glob("*.jpg")) # Retrieved the image files 
         random.shuffle(image_files) # shuffle the images randomly
-        len_all_images = len(image_files)
+        len_all_images = len(image_files) # Get the total number of images
         print(f"Total {label} images: {len_all_images}")
 
-        train_count = int(len(image_files) * train_ratio)
-        val_count = int(len(image_files) * val_ratio)
-        # test_count = int(len(image_files) * test_ratio)
+        train_count = int(len(image_files) * train_ratio) # Calculate the number of training images
+        val_count = int(len(image_files) * val_ratio) # Calculate the number of validation images
 
+        # Split the images into training, validation, and test sets if there are more than 5 images
         if len_all_images > 5:
             for i, image in enumerate(image_files):
                 if i < train_count:
@@ -53,7 +62,7 @@ def split_dataset(labels):
                 else:
                     destination_path = TEST_DATA_PATH / label / image.name
 
-                shutil.copy(str(image), str(destination_path))
+                shutil.copy(str(image), str(destination_path)) # Copy the image to the destination path
         else:
             is_small_to_split = True
             print(f"The total number of '{label}' images are too small to split, \n")
