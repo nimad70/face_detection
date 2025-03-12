e6a3b990c2d9dedb2429fcf593cfb1499ea25a61
nimadaryabar <nima.daryabar@gmail.com>
2025-03-12 08:33:58 +0100

    feat: Implemened prepare() function to preprocess the datat by resizing and data augmentation in model_training.py

--- src/model_training.py
+++ src/model_training.py
@@ -28,25 +28,70 @@ for dirs in [TRAIN_DATA_PATH, VAL_DATA_PATH, TEST_DATA_PATH]:
 IMG_SIZE = 256
 BATCH_SIZE = 32
 
+# To allow tensflow to automatically tune the buffer size for optimal performance
+AUTOTUNE = tf.data.AUTOTUNE
 
-# Loading data
-train_ds = keras.preprocessing.image_dataset_from_directory(
-    str(TRAIN_DATA_PATH),
-    image_size=(IMG_SIZE, IMG_SIZE),
-    batch_size=BATCH_SIZE
-)
+# Loading data from the dataset directory
+def load_data():
+    """
+    Load the dataset from the dataset directory.
+    
+    Returns:
+        train_ds (tf.data.Dataset): Training dataset.
+        val_ds (tf.data.Dataset): Validation dataset.
+        test_ds (tf.data.Dataset): Testing dataset.
+    """
+    train_ds = keras.preprocessing.image_dataset_from_directory(
+        str(TRAIN_DATA_PATH),
+        image_size=(IMG_SIZE, IMG_SIZE),
+        batch_size=BATCH_SIZE
+    )
 
-val_ds = keras.preprocessing.image_dataset_from_directory(
-    str(VAL_DATA_PATH),
-    image_size=(IMG_SIZE, IMG_SIZE),
-    batch_size=BATCH_SIZE
-)
+    val_ds = keras.preprocessing.image_dataset_from_directory(
+        str(VAL_DATA_PATH),
+        image_size=(IMG_SIZE, IMG_SIZE),
+        batch_size=BATCH_SIZE
+    )
 
-test_ds = keras.preprocessing.image_dataset_from_directory(
-    str(TEST_DATA_PATH),
-    image_size=(IMG_SIZE, IMG_SIZE),
-    batch_size=BATCH_SIZE
-)
+    test_ds = keras.preprocessing.image_dataset_from_directory(
+        str(TEST_DATA_PATH),
+        image_size=(IMG_SIZE, IMG_SIZE),
+        batch_size=BATCH_SIZE
+    )
 
-class_names = train_ds.class_names
-print("Class Names:", class_names)
+    class_names = train_ds.class_names
+    print("Class Names:", class_names)
+
+    return train_ds, val_ds, test_ds
+
+
+# Apply data pre-processing to process train, valid, and test sets
+def prepare(ds, shuffle=False, augment=False):
+    # Resize datasets
+    ds = ds.map(lambda x, y: (tf.image.resize(x, IMG_SIZE), y), num_parallel_calls=AUTOTUNE)
+
+    if shuffle:
+        ds = ds.shuffle(1000)
+
+    ds = ds.batch(BATCH_SIZE)
+
+    if augment:
+        # Randomly flip images horizontally
+        ds = ds.map(lambda x, y: (tf.image.random_flip_left_right(x), y), num_parallel_calls=AUTOTUNE) 
+        
+        # Randomly rotate images
+        ds = ds.map(lambda x, y: (tf.image.random_brightness(x, max_delta=0.3), y), num_parallel_calls=AUTOTUNE)
+        
+        # Randomly adjust the contrast of images
+        ds = ds.map(lambda x, y: (tf.image.random_contrast(x, lower=0.5, upper=1.3), y), num_parallel_calls=AUTOTUNE)
+        
+        # Randomly adjust the hue of images
+        ds = ds.map(lambda x, y: (tf.image.random_hue(x, max_delta=0.05), y), num_parallel_calls=AUTOTUNE)
+
+        # Randomly adjust the saturation of images
+        ds = ds.map(lambda x, y: (tf.image.random_saturation(x, lower=0.5, upper=3), y), num_parallel_calls=AUTOTUNE)
+
+    # Use buffered prefetching to ensures the next batch is prepared while the current batch is being processed
+    return ds.prefetch(buffer_size=AUTOTUNE)
+
+
