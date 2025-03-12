ce991d59c8a631ff2dd7869df76b3a809cebd395
nimadaryabar <nima.daryabar@gmail.com>
2025-03-12 16:48:25 +0100

    Initialiazed customized face detection using the fine-tuned model, cutomized_face_detection.py

---
+++ src/customized_face_detection.py
@@ -0,0 +1,11 @@
+# new_face_detection.py
+import cv2
+import tensorflow as tf
+import numpy as np
+from src.face_detection import video_capture, haarcascade_classifier, preprocessing_frame, scale_bounding_box
+from src.model_evaluation import load_model
+
+
+
+if __name__ == "__main__":
+    pass
