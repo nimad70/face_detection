399d9d7fd389c0b84a5ef09eae7da4781a4bd8d9
nimadaryabar <nima.daryabar@gmail.com>
2025-03-30 23:41:02 +0200

    feat: Modified imported modules, utils and config, by including 'src' in their path to use them inside the app.py in resNet_face_detector.py

--- src/resNet_face_detector.py
+++ src/resNet_face_detector.py
@@ -6,8 +6,10 @@ It also supports GPU acceleration for faster processing.
 
 import cv2
 import numpy as np
-from utils import update_fps
-from config import resNetSSD_config
+# from utils import update_fps
+# from config import resNetSSD_config
+from src.utils import update_fps
+from src.config import resNetSSD_config
 
 # Initialize model
 def load_detector(prototxt, caffemodel, gpu_enabled):
