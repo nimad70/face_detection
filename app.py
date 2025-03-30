2cb2c36ff9964bcec99dbfdab93847b423e717ee
nimadaryabar <nima.daryabar@gmail.com>
2025-03-30 10:17:45 +0200

    refactor: applied modifications after seperating show_menu() and mani() functions from app.py

--- app.py
+++ app.py
@@ -6,70 +6,14 @@ create labeled datasets, apply data augmentation, train and fine-tune a model, e
 and perform real-time face detection and smile classification.
 """
 
-import cv2
-import sys
-from src.face_detection import video_capture, haarcascade_classifier, face_detector, draw_rectangle, preprocessing_frame
+from src.menu import show_menu
+from src.face_detection import main
 from src.face_detection_multi_threading import start_threads
 from src.dataset_creation import split_dataset, LABELS
 from src.data_augmentation import save_augmented_images
 from src.model_training import train_model, fine_tune_model
 from src.model_evaluation import display_accuracy_metrics, plot_confusuion_matrix
 from src.customized_face_detection import customized_face_detection
-
-
-def main():
-    """
-    Captures video from the webcam, detects faces, and displays the processed frames with bounding boxes from 
-    face_detection.py module.
-    
-    The function continuously reads frames from the webcam, preprocesses them, detects faces using Haar Cascade classifier,
-    and draws bounding boxes around detected faces. The video stream is displayed in a window, and the loop exits
-    when the user presses 'q'.
-    """
-    cap = video_capture()
-    classifier = haarcascade_classifier()
-
-    while True:
-        res, frame = cap.read() # capture frames from the video source
-
-        # If the frame is not captured, then break the loop
-        if not res:
-            print("Failed to capture frames")
-            break
-
-        preprocessed_frame = preprocessing_frame(frame)
-        face_classifier = haarcascade_classifier()
-        detected_faces = face_detector(face_classifier, preprocessed_frame)
-        draw_rectangle(frame, detected_faces)
-
-        cv2.imshow("Webcam", frame) # Display the captured frame
-
-        # Exit if the user presses 'q'
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # Release the webcam and close the window
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-def show_menu():
-    """
-    Displays the main menu options for the user.
-
-    The user can select various functionalities such as capturing a face, 
-    dataset creation, data augmentation, model training, fine-tuning, evaluation, 
-    and real-time face detection.
-    """
-    print("\nPlease select an option from the menu:")
-    print("1. Capture face")
-    print("2. Capture and Split Dataset")
-    print("3. Save Augmented Images")
-    print("4. Train Model")
-    print("5. Fine-tune Model")
-    print("6. Evaluate Model")
-    print("7. Real-time Face Detection and Smile Classification")
-    print("8. Exit")
 
 
 if __name__ == "__main__":
