3fe9651196a1c9710e5465600982dfae4558bf1f
nimadaryabar <nima.daryabar@gmail.com>
2025-04-02 23:21:31 +0200

    feat: Implemented face detection using resNet Module in multi_processing_main.py

--- src/multi_processing/multi_processing_main.py
+++ src/multi_processing/multi_processing_main.py
@@ -1,9 +1,19 @@
 import cv2
 from src.multi_processing.video_stearm import VideoStream
-from src.object_detection.resNet_face_detector import run_face_detection
+from src.object_detection.resNet_face_detector import load_detector, detect_faces, draw_rectangle
+from src.utils.config import resNetSSD_config
 
-def main():
+
+def multi_processing_main():
+    """
+    Main function to run the multi-processing face detection using ResNet model.
+    """
+    # Initialize video stream and load the model
     vs = VideoStream().start()
+
+    # Load the ResNet model for face detection
+    gpu_enabled, prototxt, caffemodel, confidence_threshold, img_size = resNetSSD_config()
+    net = load_detector(prototxt, caffemodel, gpu_enabled)
 
     while True:
         ret, frame = vs.read()
@@ -11,8 +21,9 @@ def main():
             print("Failed to grab frame")
             break
 
-        # Face detection
-        run_face_detection(frame)
+        # Detect faces in the frame using the ResNet model
+        faces = detect_faces(frame, net, confidence_threshold, img_size)
+        draw_rectangle(frame, faces)
 
         cv2.imshow("Frame", frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
