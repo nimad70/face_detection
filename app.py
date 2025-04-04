c4b38bc25fdee52f242f6cc058cd20a78394506b
nimadaryabar <nima.daryabar@gmail.com>
2025-04-04 16:06:55 +0200

    feat: Modified the app.py to only process the available option inside the menu

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from src.object_detection.resNet_face_detector import run_face_detection
 from src.object_detection.mobileNet_object_detector import run_object_detection
 from src.object_detection.customized_face_detection import customized_face_detection
 from src.multi_processing.multi_processing_main import multi_processing_main
+from src.multi_processing.threaded_main import thread_main
 from src.multi_processing.face_detection_multi_threading import start_threads
 from src.dataset.dataset_creation import split_dataset, LABELS
 from src.dataset.data_augmentation import save_augmented_images
@@ -22,106 +23,79 @@ from src.model_training.model_evaluation import display_accuracy_metrics, plot_c
 if __name__ == "__main__":
     while True:
         show_menu()
-        choice = input("Enter your choice (1-12): ")
+        choice = input("Enter your choice (1-7): ")
 
         if choice == "1":
-            """
-            Captures images. OpenCV Haar Cascade Face Detector
-            """
-            haar_cascade_face_detector()
-        
-        elif choice == "2":
-            """
-            Captures images. OpenCV DNN + ResNet-Based Face Detector
-            """
-            run_face_detection()
-        
-        elif choice == "3":
-            """
-            Captures images. Person detector (MobileNetSSD)
-            """
-            run_object_detection(is_object_detection=False)
-        
-        elif choice == "4":
-            """
-            Captures images. Object detector (MobileNetSSD)
-            """
-            run_object_detection(is_object_detection=True)
-
-        elif choice == "5":
-            """
-            Captures images, Face detection (ResNet/Multi-processing)
-            """
-            multi_processing_main()
-
-        elif choice == "6":
             """
             Captures images, splits them into training, validation, and testing datasets.
             Ensures that a minimum of 6 images per category are taken before proceeding.
             """
+            thread_main()
+
             is_splited = True
             while is_splited:
                 start_threads()
                 res = split_dataset(LABELS) # Split the dataset
                 if res:
-                    print("Smile and Non-smile datasets are splited")
+                    print("Smile and Non-smile datasets are created.")
                     break
-                warning_text = "*WARNING: Please take at least 6 shots per each category to add to the dataset"
+                warning_text = "*WARNING: Please take at least 6 shots per each category to add to the dataset!"
                 print('-' * len(warning_text))
                 print(f"{warning_text}")
                 print('-' * len(warning_text))
         
-        elif choice == "7":
-            print("6. Capture and Split Dataset (Multi-Multi-processing)")
-
-        elif choice == "8":
+        elif choice == "2":
             """
             Applies data augmentation techniques to the training dataset to enhance generalization.
             """
-            save_augmented_images()
-            print("Augmented images are saved to the corresponding training datasets")
+            print("\n => If you want to augment the dataset, please make sure that you have already created the dataset,")
+            print(" => By choosing this option, data augmentation inside model training will be skipped.")
+            is_augmented = False
+            is_augmented = save_augmented_images()
+            print("Augmented images are saved to the corresponding training datasets!")
 
-        elif choice == "9":
+        elif choice == "3":
             """
             Trains the model using the prepared dataset.
             """
-            train_model()
-            print("Model training is completed")
+            train_model(is_augmented)
+            print("Model training is completed!")
         
-        elif choice == "10":
+        elif choice == "4":
             """
             Fine-tunes the pre-trained model to improve performance.
             """
             fine_tune_model()
-            print("Model fine-tuning is completed")
+            print("Model fine-tuning is completed!")
 
-        elif choice == "11":
+        elif choice == "5":
             """
             Evaluates the trained or fine-tuned model using accuracy, precision, recall, 
             F1-score, and confusion matrix.
             """
             display_accuracy_metrics(is_fine_tuned=True)
             plot_confusuion_matrix()
-            print("Model evaluation is completed")
+            print("Model evaluation is completed!")
         
-        elif choice == "12":
+        elif choice == "6":
             """
             Runs real-time face detection and smile classification using a webcam feed.
             """
             customized_face_detection()
 
-        elif choice == "13":
+        elif choice == "7":
             """
             Exits the application.
             """
-            print("Exiting the application!")
+            print("Thank you!")
+            print("Exiting the application...")
             break
         
         else:
             """
             Handles invalid menu selections.
             """
-            invalid_option_text = "Invalid choice. Please select a valid option between 1 to 8."
+            invalid_option_text = "<Invalid choice> --> Please select a valid option between 1 to 7!"
             print('-' * len(invalid_option_text))
             print(f"{invalid_option_text}")
             print('-' * len(invalid_option_text))
