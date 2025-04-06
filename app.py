c9d20d88b8f6a37ff69806d38f856770cecde676
nimadaryabar <nima.daryabar@gmail.com>
2025-04-06 11:32:40 +0200

    feat: Removed unnecessory imported moduldes. Updated the first two options to create a dataset and image augmentation in app.py

--- app.py
+++ app.py
@@ -7,24 +7,26 @@ and perform real-time face detection and smile classification.
 """
 
 from src.utils.menu import show_menu
-from src.object_detection.haar_cascade_face_detector import haar_cascade_face_detector
-from src.object_detection.resNet_face_detector import run_face_detection
-from src.object_detection.mobileNet_object_detector import run_object_detection
 from src.object_detection.customized_face_detection import customized_face_detection
-from src.multi_processing.multi_processing_main import multi_processing_main
-from src.multi_processing.face_detection_resNet_threaded_main import thread_main
-from src.multi_processing.face_detection_multi_threading import start_threads
 from src.multi_processing.threaded_data_pipeline_resNet import data_pipeline_thread
-from src.data_pipeline.dataset_creation import split_dataset, LABELS
-from src.data_pipeline.data_augmentation import save_augmented_images
+from data_pipeline.tarining_data_pipeline import split_dataset, LABELS
+from data_pipeline.augment_data_pipeline import save_augmented_images
 from src.model_training.model_training import train_model, fine_tune_model
 from src.model_training.model_evaluation import display_accuracy_metrics, plot_confusuion_matrix
-
+# from src.object_detection.haar_cascade_face_detector import haar_cascade_face_detector
+# from src.object_detection.resNet_face_detector import run_face_detection
+# from src.object_detection.mobileNet_object_detector import run_object_detection
+# from src.multi_processing.multi_processing_main import multi_processing_main
+# from src.multi_processing.face_detection_resNet_threaded_main import thread_main
+# from src.multi_processing.face_detection_multi_threading import start_threads
 
 if __name__ == "__main__":
+    # Initialize variables
+    is_augmented = False
+
     while True:
         show_menu()
-        choice = input("Enter your choice (1-7): ")
+        choice = input("Enter your choice (1-7): ").strip()
 
         if choice == "1":
             """
@@ -33,35 +35,37 @@ if __name__ == "__main__":
             """
             is_splited = True
             while is_splited:
-                data_pipeline_thread()
-                res = split_dataset(LABELS) # Split the dataset
-                if res:
-                    print("Smile and Non-smile datasets are created.")
-                    break
-                warning_text = "*WARNING: Please take at least 6 shots per each category to add to the dataset!"
-                print('-' * len(warning_text))
-                print(f"{warning_text}")
-                print('-' * len(warning_text))
-                try:
-                    exit_request = input("\nDo you want to exit? (y/n): ").strip().lower()
-                    if exit_request == 'y':
-                        print("Exiting the dataset creation...")
-                        is_splited = False
+                data_created = data_pipeline_thread()
+                if data_created:
+                    res = split_dataset(LABELS) # Split the dataset
+                    if res:
+                        print("[INFO] Smile and Non-smile datasets are created.")
                         break
-                    elif exit_request == 'n':
-                        print("Continuing to take more shots...")
-                except ValueError:
-                    print("Invalid input. Please enter 'y' or 'n'.")
+                    warning_text = "[WARNING] Please take at least 6 shots per each category to add to the dataset!"
+                    print('-' * len(warning_text))
+                    print(f"{warning_text}")
+                    print('-' * len(warning_text))
+                    try:
+                        exit_request = input("\nDo you want to exit? (y/n): ").strip().lower()
+                        if exit_request == 'y':
+                            print("Exiting the dataset creation...")
+                            is_splited = False
+                            break
+                        elif exit_request == 'n':
+                            print("Continuing to take more shots...")
+                    except ValueError:
+                        print("[WARNING] Invalid input. Please enter 'y' or 'n'.")
+                else:
+                    print("Exiting...")
+                    break
         
         elif choice == "2":
             """
             Applies data augmentation techniques to the training dataset to enhance generalization.
             """
-            print("\n => If you want to augment the dataset, please make sure that you have already created the dataset,")
-            print(" => By choosing this option, data augmentation inside model training will be skipped.")
-            is_augmented = False
+            print("\n[INFO] 1) If you want to augment the dataset, please make sure you already have created the dataset.")
+            print("[INFO] 2) By choosing this option, data augmentation inside model training will be skipped.\n")
             is_augmented = save_augmented_images()
-            print("Augmented images are saved to the corresponding training datasets!")
 
         elif choice == "3":
             """
